02b2d350f90cd813703d572bbfc7e0c65ad70804
José María Muñoz Quijada <JoseMariaMQ@users.noreply.github.com>
2021-02-18 16:00:56 +0100

    Añadir datos a la base de datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import {store} from "./firebaseconf";
 
 function App() {
 
@@ -7,15 +8,27 @@ function App() {
   const [error, setError] = useState("")
   const [usuario, setUsuario] = useState([])
 
-  const setUsuarios = (e) => {
+  const setUsuarios = async (e) => {
     e.preventDefault()
     if (!nombre.trim()) {
       setError("El campo nombre está vacío")
-      return
-    }
-    if (!phone.trim()) {
+    } else if (!phone.trim()) {
       setError("El campo teléfono está vacío")
     }
+
+    const usuario = {
+      nombre: nombre,
+      telefono: phone
+    }
+    try {
+      const data = await store.collection('agenda').add(usuario)
+      console.log('Tarea añadira')
+    } catch (e) {
+      console.log(e)
+    }
+    setNombre('')
+    setPhone('')
+    setError('')
   }
 
   return (
@@ -24,8 +37,8 @@ function App() {
         <div className="col">
           <h2>Formulario de Usuarios</h2>
           <form onSubmit={setUsuarios} className="form-group">
-            <input onChange={(e) => {setNombre(e.target.value)}} className="form-control" placeholder="Introduce el nombre" type="text"/>
-            <input onChange={(e) => {setPhone(e.target.value)}} className="form-control mt-3" placeholder="Introduce el número" type="text"/>
+            <input value={nombre} onChange={(e) => {setNombre(e.target.value)}} className="form-control" placeholder="Introduce el nombre" type="text"/>
+            <input value={phone} onChange={(e) => {setPhone(e.target.value)}} className="form-control mt-3" placeholder="Introduce el número" type="text"/>
             <input className="btn btn-dark btn-block mt-3" type="submit" value="Registrar"/>
           </form>
           {
