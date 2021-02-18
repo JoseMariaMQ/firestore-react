9faf2655c6cf9a29301bc7c95d6f5cbc4f1c5f48
José María Muñoz Quijada <JoseMariaMQ@users.noreply.github.com>
2021-02-18 17:16:35 +0100

    Actualizar datos de la base de datos

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import {store} from "./firebaseconf";
 
 function App() {
 
+  const [modoEdicion, setModoEdicion] = useState(null)
+  const [idUsuario, setIdUsuario] = useState("")
   const [nombre, setNombre] = useState("")
   const [phone, setPhone] = useState("")
   const [error, setError] = useState("")
@@ -29,7 +31,7 @@ function App() {
         telefono: phone
       }
       try {
-        const data = await store.collection('agenda').add(usuario)
+        await store.collection('agenda').add(usuario)
         const {docs} = await  store.collection('agenda').get()
         const nuevoArray = docs.map(item => ({id: item.id, ...item.data()}))
         setUsuarios(nuevoArray)
@@ -54,15 +56,63 @@ function App() {
     }
   }
 
+  const actualizarUsuario = async (id) => {
+    try {
+      const data = await store.collection('agenda').doc(id).get()
+      const {nombre, telefono} = data.data()
+      setNombre(nombre)
+      setPhone(telefono)
+      setIdUsuario(id)
+      setModoEdicion(true)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const setUpdate = async (e) => {
+    e.preventDefault()
+    if (!nombre.trim()) {
+      setError("El campo nombre está vacío")
+    } else if (!phone.trim()) {
+      setError("El campo teléfono está vacío")
+    } else {
+      const userUpdate = {
+        nombre: nombre,
+        telefono: phone
+      }
+      try {
+        await store.collection('agenda').doc(idUsuario).set(userUpdate)
+        const {docs} = await  store.collection('agenda').get()
+        const nuevoArray = docs.map(item => ({id: item.id, ...item.data()}))
+        setUsuarios(nuevoArray)
+        alert('Usuario actualizado')
+        setNombre('')
+        setPhone('')
+        setError('')
+        setModoEdicion(false)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+
   return (
     <div className="container">
       <div className="row">
         <div className="col">
           <h2>Formulario de Usuarios</h2>
-          <form onSubmit={setUsuario} className="form-group">
+          <form onSubmit={modoEdicion ? setUpdate : setUsuario} className="form-group">
             <input value={nombre} onChange={(e) => {setNombre(e.target.value)}} className="form-control" placeholder="Introduce el nombre" type="text"/>
             <input value={phone} onChange={(e) => {setPhone(e.target.value)}} className="form-control mt-3" placeholder="Introduce el número" type="text"/>
-            <input className="btn btn-dark btn-block mt-3" type="submit" value="Registrar"/>
+            {
+              modoEdicion ?
+                  (
+                      <input className="btn btn-dark btn-block mt-3" type="submit" value="EDITAR"/>
+                  ) :
+                  (
+                      <input className="btn btn-dark btn-block mt-3" type="submit" value="REGISTRAR"/>
+                  )
+            }
           </form>
           {
             error ?
@@ -83,8 +133,9 @@ function App() {
               usuarios.length !== 0 ?
               (
               usuarios.map(item => (
-                  <li className="row list-group-item" key={item.id}>Nombre: {item.nombre} -- Tel: {item.telefono}
-                  <button onClick={(id) => {borrarUsuario(item.id)}} className="btn btn-danger float-right">BORRAR</button>
+                  <li className="row list-group-item" key={item.id}>{item.nombre} -- {item.telefono}
+                    <button onClick={(id) => {borrarUsuario(item.id)}} className="btn btn-danger float-right">BORRAR</button>
+                    <button onClick={(id) => {actualizarUsuario(item.id)}} className="btn btn-info mr-2 float-right">ACTUALIZAR</button>
                   </li>
               ))
               ) :
